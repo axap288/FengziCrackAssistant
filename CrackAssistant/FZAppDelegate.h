80ca14ebd9dd98ff0441ad394ab0d1192f1c7ee0
LiuNian <superliunian@gmail.com>
2014-04-14 21:05:32 +0800

    first commit!

---
+++ CrackAssistant/FZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  fzAppDelegate.h
+//  CrackAssistant
+//
+//  Created by LiuNian on 14-4-9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
